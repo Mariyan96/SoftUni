1f576f9c5cc39f5ff0b6087458621a8d59324584
Mariyan96 <37500270+Mariyan96@users.noreply.github.com>
2018-08-16 10:32:27 +0300

    Adding JS Projects

--- JavaScript/JS Core/JS Applications/EXAM/EXAM PREP 20 August 2017/02.Point of Sale/scripts/app.js
+++ JavaScript/JS Core/JS Applications/EXAM/EXAM PREP 20 August 2017/02.Point of Sale/scripts/app.js
@@ -1,18 +1,25 @@
-let handlers={};
-$(()=>{
-    const app = Sammy('#container',function () {
-        this.use('Handlebars','hbs');
-        this.get('#/home',handlers.getWelcomePage);
-        this.get('/index.html',handlers.getWelcomePage);
-        this.post('#/register',handlers.registerUser);
-        this.post('#/login',handlers.loginUser);
-        this.get('#/logout',handlers.logoutUser);
-        this.get('#/editor',handlers.getEditor);
-        this.post('#/create/entry',handlers.createEntry);
-        this.post('#/remove/entry',handlers.removeEntry);
-        this.post('#/checkout',handlers.checkOut);
-        this.get('#/overview',handlers.getMyReceipts);
-        this.get('#/receipt/details/:id',handlers.getReceiptDetails);
+const handlers = {};
+
+$(() => {
+    // Define routes here using Sammy.js
+    const app = Sammy('#container', function () {
+        this.use('Handlebars', 'hbs');
+
+        this.get('index.html', handlers.getWelcomePage);
+        this.get('#/home', handlers.getWelcomePage);
+
+        this.post('#/register', handlers.registerUser);
+        this.post('#/login', handlers.loginUser);
+        this.get('#/logout', handlers.logout);
+
+        this.get('#/editor', handlers.getEditor);
+        this.post('#/entry/create', handlers.createEntry);
+        this.post('#/entry/delete', handlers.deleteEntry);
+        this.post('#/checkout', handlers.checkout);
+
+        this.get('#/overview', handlers.getMyReceipts);
+        this.get('#/receipt/details/:id', handlers.getReceiptDetails);
     });
+
     app.run();
 });
--- JavaScript/JS Core/JS Applications/EXAM/EXAM PREP 20 August 2017/02.Point of Sale/scripts/remote.js
+++ JavaScript/JS Core/JS Applications/EXAM/EXAM PREP 20 August 2017/02.Point of Sale/scripts/remote.js
@@ -1,45 +1,48 @@
-let remote=(()=>{
+let remote = (() => {
     const BASE_URL = 'https://baas.kinvey.com/';
-    const APP_KEY='kid_ry_6TMnrQ';
-    const APP_SECRET='70d998cf8a614c3d9807fd165cf968a2';
+    const APP_KEY = 'kid_ry_6TMnrQ';
+    const APP_SECRET = '70d998cf8a614c3d9807fd165cf968a2';
 
-    function makeAuthHeader(auth) {
-        if (auth === 'Basic') {
+    function makeAuth(auth) {
+        if (auth === 'basic') {
             return `Basic ${btoa(APP_KEY + ":" + APP_SECRET)}`;
         } else {
-            return `Kinvey ${sessionStorage.getItem('authToken')}`
+            return `Kinvey ${sessionStorage.getItem('authtoken')}`
         }
     }
 
-    function makeRequest(method,module,endpoint,auth) {
+    function makeRequest(method, module, endpoint, auth) {
         return {
             url: BASE_URL + module + '/' + APP_KEY + '/' + endpoint,
             method: method,
             headers: {
-                'Authorization': makeAuthHeader(auth)
+                'Authorization': makeAuth(auth)
             }
         }
     }
-    function get(module, endpoint, auth) {
+
+    function get (module, endpoint, auth) {
         return $.ajax(makeRequest('GET', module, endpoint, auth));
     }
-    function post(module,endpoint,auth,data) {
-        let obj =makeRequest('POST',module,endpoint,auth)
-        if(data){
-           obj.data = data;
+
+    function post (module, endpoint, auth, data) {
+        let obj = makeRequest('POST', module, endpoint, auth);
+        if (data) {
+            obj.data = data;
         }
         return $.ajax(obj);
     }
-    function update(module,endpoint,auth,data) {
-        let obj = makeRequest('PUT',module,endpoint,auth);
-        if(data){
-            obj.data=data;
-        }
-        return $.ajax(obj)
+
+    function update(module, endpoint, auth, data) {
+        let obj = makeRequest('PUT', module, endpoint, auth);
+        obj.data = data;
+        return $.ajax(obj);
     }
-    function remove(module,endpoint,auth) {
-        return $.ajax(makeRequest('DELETE',module,endpoint,auth));
+
+    function remove(module, endpoint, auth) {
+        return $.ajax(makeRequest('DELETE', module, endpoint, auth));
     }
+
     return {
         get,
         post,
